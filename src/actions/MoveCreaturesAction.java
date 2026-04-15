f91167b8e327fea31b5547fe6124206b79be38df
Alena Bormatova <alena.petryuk@yandex.ru>
2026-04-15 21:51:12 +0700

    refactor MoveCreaturesAction: extract creature filtering helpers

--- src/actions/MoveCreaturesAction.java
+++ src/actions/MoveCreaturesAction.java
@@ -1,9 +1,11 @@
 package actions;
 
+import entity.Coordinates;
 import entity.Creature;
 import entity.Entity;
 import world.WorldMap;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,26 +13,37 @@ public final class MoveCreaturesAction implements Action {
 
     @Override
     public void execute(WorldMap worldMap) {
-        List<WorldMap.PositionedEntity> positionedEntities =
-                worldMap.getPositionedEntities();
+        List<WorldMap.PositionedEntity> positionedCreatures = getPositionedCreatures(worldMap);
 
-        for (WorldMap.PositionedEntity positionedEntity : positionedEntities) {
-            Entity entity = positionedEntity.entity();
-
-            if (!(entity instanceof Creature creature)) {
-                continue;
-            }
+        for (WorldMap.PositionedEntity positionedCreature : positionedCreatures) {
+            Creature creature = (Creature) positionedCreature.entity();
 
             if (!creature.isAlive()) {
                 continue;
             }
 
-            Optional<Entity> actualEntity = worldMap.get(positionedEntity.position());
-            if (actualEntity.isEmpty() || actualEntity.orElseThrow() != creature) {
+            if (!isStillAtPosition(worldMap, positionedCreature.position(), creature)) {
                 continue;
             }
 
             creature.makeMove(worldMap);
         }
+    }
+
+    private List<WorldMap.PositionedEntity> getPositionedCreatures(WorldMap worldMap) {
+        List<WorldMap.PositionedEntity> positionedCreatures = new ArrayList<>();
+
+        for (WorldMap.PositionedEntity positionedEntity : worldMap.getPositionedEntities()) {
+            if (positionedEntity.entity() instanceof Creature) {
+                positionedCreatures.add(positionedEntity);
+            }
+        }
+
+        return positionedCreatures;
+    }
+
+    private boolean isStillAtPosition(WorldMap worldMap, Coordinates position, Creature creature) {
+        Optional<Entity> actualEntity = worldMap.get(position);
+        return actualEntity.isPresent() && actualEntity.orElseThrow() == creature;
     }
 }
